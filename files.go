e12896f52ea4a3225b565582be4fc5629141530d
Tillo <tillo.bosshart@gmail.com>
2019-12-05 15:11:10 +0100

    returns error if file already exists

--- files.go
+++ files.go
@@ -1,6 +1,7 @@
 package helferlein
 
 import (
+	"errors"
 	"io"
 	"net/http"
 	"os"
@@ -9,24 +10,30 @@ import (
 // DownloadFiles downloads a file to a given path
 func DownloadFile(url string, filepath string) error {
 	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
+	if _, err := os.Stat(filepath); err == nil {
+		return errors.New("File already exists")
+	} else if os.IsNotExist(err) {
+		out, err := os.Create(filepath)
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+
+		// Get the data
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		// Write the body to file
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	} else {
 		return err
 	}
-	defer out.Close()
-
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
